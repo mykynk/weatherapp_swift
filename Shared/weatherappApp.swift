d842c2f38d4b3ee5ec56da87e96eac8f431bbaa2
Muhammed Yusuf Kaynak <mykynk1@gmail.com>
2022-08-07 13:43:59 +0300

    Initial Commit

---
+++ Shared/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  Shared
+//
+//  Created by Muhammed Yusuf Kaynak on 7.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
